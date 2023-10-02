008f363516b7d1c2007ccee76c83d8bc58ec444b
blahoo <117117475+blahoo@users.noreply.github.com>
2023-10-02 17:11:36 -0400

    hide win

--- klog.cpp
+++ klog.cpp
@@ -1,26 +1,49 @@
 #include <iostream>
 #include <fstream>
+#include <windows.h>
 
 
 int main(){
-    // open or create a text file in append mode
-    std::ofstream keylog("log.txt", std::ios::app);
 
-    // if the file didn't open, shut the program down
-    if (!keylog.is_open()){
-        return 1;
-    }
+    // Get the console window handle
+    HWND consoleWindow = GetConsoleWindow(); 
+
+    // Check if console window handle retrieved
+    if (consoleWindow != NULL) {
+
+        // Hide the console window
+        ShowWindow(consoleWindow, SW_HIDE);
+
+        // open or create a text file in append mode
+        std::ofstream keylog("log.txt", std::ios::app);
+
+        // if the file didn't open, shut the program down
+        if (!keylog.is_open()){
+            return 1;
+        }
+
+    
 
 
 
-    keylog << "hello world";
+
+            
+
+        // Add a delay to avoid high CPU usage (you can adjust this)
+        Sleep(100);
+        
 
 
-    std::cout << "hello world";
+
+        keylog << "hello world";
+
+
+        std::cout << "hello world";
 
 
 
-    keylog.close();
+        keylog.close();
+    }else{ return 1; }
 
     return 0;
 }
